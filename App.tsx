8e5edc0db20bccf3eaa66a3cb0ee3f0857061328
jainl28patel <jainilpatel2003@gmail.com>
2022-12-03 04:45:39 +0530

    Login signup and Registeration page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <Dashboard />
   );
 }
 
